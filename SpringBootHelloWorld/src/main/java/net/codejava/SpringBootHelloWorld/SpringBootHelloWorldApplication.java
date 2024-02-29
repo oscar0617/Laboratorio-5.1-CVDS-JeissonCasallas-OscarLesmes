9d61d96b7380d98f0888e0626779455c3973fe70
oscar0617 <oscarsantiagolesmes@gmail.com>
2024-02-29 17:16:02 -0500

    springboot

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -2,8 +2,11 @@ package net.codejava.SpringBootHelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@SpringBootApplication
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,5 @@ public class SpringBootHelloWorldApplication {
 	public String home(){
 		return "Hello world Spring Boot";
 	}
-
+	
 }
