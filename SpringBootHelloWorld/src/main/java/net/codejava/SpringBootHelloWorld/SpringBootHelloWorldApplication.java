94734687b8469ea9d9279995c2aa9c612de24d6f
oscar0617 <oscarsantiagolesmes@gmail.com>
2024-02-29 16:59:37 -0500

    Controller

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -3,11 +3,16 @@ package net.codejava.SpringBootHelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@RestController
 public class SpringBootHelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home(){
+		return "Hello world Spring Boot";
 	}
 
 }
