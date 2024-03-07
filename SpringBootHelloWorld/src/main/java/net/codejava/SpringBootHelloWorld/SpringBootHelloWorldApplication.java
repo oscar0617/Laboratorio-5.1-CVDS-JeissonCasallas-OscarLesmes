466fdae1dbc2ff95725c74c7d5dd79080d0e1a4d
oscar0617 <oscarsantiagolesmes@gmail.com>
2024-03-06 22:14:39 -0500

    final

--- SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ SpringBootHelloWorld/src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -3,6 +3,7 @@ package net.codejava.SpringBootHelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,7 @@ public class SpringBootHelloWorldApplication {
         return "<h2>This is a new message</h2>";
     }
 
-    @CrossOrigin
+    @CrossOrigin(origins = "http://localhost:3000/api/hello", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
     @GetMapping("/api/hello")
     public String newMessageCors(){
         return "<h2>Hello world Cors!</h2>";
